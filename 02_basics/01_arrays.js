aee74c1f2e0b21a52ddf608f1e64f6787b541d37
rahulkpr2510 <rahulkpr2510@gmail.com>
2024-10-31 14:33:59 +0000

    arrays_01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array Mathods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) // used to push the element to the start of the array
+// myArr.shift() // used to pop element from the start of the array
+
+// console.log(myArr.indexOf(9));
+// console.log(myArr.includes(9));
+
+const newArr = myArr.join() // adds all the elements of the array into a string
+
+// console.log(myArr)
+
+// slice, splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C ",myArr)
