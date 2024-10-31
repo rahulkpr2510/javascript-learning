50d61eba8b19463869b72b5465934d72b2204c72
rahulkpr2510 <rahulkpr2510@gmail.com>
2024-10-31 05:44:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
