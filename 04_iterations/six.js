ad196f8f511ad48105ea941578727e760181366e
rahulkpr2510 <rahulkpr2510@gmail.com>
2024-11-02 14:37:17 +0000

    filter map reduce

---
+++ 04_iterations/six.js
@@ -0,0 +1,13 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item;
+// })
+
+// forEach loop doesn't return any value
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const nums = myNums.filter( (num) => num > 4 )
+console.log(nums);
